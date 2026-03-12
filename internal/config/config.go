160fbda06870cf6541dcaa03e44ab4da426b1749
unsubstantiated-Script <jusfulton@gmail.com>
2026-03-12 14:36:05 -0500

    Config setup

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import "time"
+
+type Config struct {
+	AppPort       string
+	RedisAddr     string
+	SQLiteDSN     string
+	ProductTTL    time.Duration
+	RecentListKey string
+	RecentLimit   int64
+}
+
+func Load() Config {
+	return Config{
+		AppPort:       ":8080",
+		RedisAddr:     "localhost:6379",
+		SQLiteDSN:     "app.db",
+		ProductTTL:    time.Minute,
+		RecentListKey: "recent_products",
+		RecentLimit:   10,
+	}
+}
