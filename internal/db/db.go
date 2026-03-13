feca32e5d944af47761757315262a65a0efdace3
unsubstantiated-Script <jusfulton@gmail.com>
2026-03-13 18:36:47 -0500

    Database

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,39 @@
 package db
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"goCachedAPI/internal/models"
+)
+
+func New(sqliteDSN string) *gorm.DB {
+	database, err := gorm.Open(sqlite.Open(sqliteDSN), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("failed to connect to sqlite: %v", err)
+	}
+
+	seed(database)
+
+	return database
+}
+
+func seed(database *gorm.DB) {
+	var count int64
+
+	if err := database.Model(&models.Product{}).Count(&count).Error; err != nil {
+		log.Fatalf("failed to count products: %v", err)
+	}
+
+	if count == 0 {
+		if err := database.Create(&models.Product{
+			Name:  "Product1",
+			Price: 100,
+		}).Error; err != nil {
+			log.Fatalf("failed to seed products: %v", err)
+		}
+	}
+}
